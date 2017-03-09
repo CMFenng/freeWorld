8f615f2f15879ef101d44619e98545abb7998954
mingfengst <mingfengst@163.com>
2017-03-09 20:26:41 +0800

    Commit by CMF

---
+++ askPush.bat
@@ -0,0 +1,7 @@
+e:
+cd GitHub\test
+set /p txt=ÄãÊÇË­£º
+git add --all
+git commit -m "Commit by %txt%"
+git push
+pause
---
+++ autoPush.bat
@@ -0,0 +1,5 @@
+e:
+cd GitHub\test
+git add --all
+git commit -m "Commit by CMF"
+git push
