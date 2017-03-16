76529b11eb52a2d2464092ac4926cecb5ac5e0b4
mingfengst <mingfengst@163.com>
2017-03-16 19:09:34 +0800

    Commit by CMF

--- freeWorld/js/cityWalk.js
+++ freeWorld/js/cityWalk.js
@@ -1,3 +1,37 @@
 /**
  * Created by CMF on 2017/3/16.
  */
+$(function () {
+    $.ajax({
+        url : "json/citywalk/cityWalkList.json",
+        success : function (data, staus, jqxhr) {
+            handleListData(data);
+        }
+    });
+    
+    // 处理数据
+    function handleListData(result){
+        for(var i = 0; i < result.length; i++){
+            // 创建类名为 listUnit 的 div
+            var $listUnitDiv = $("<div class='listUnit'></div>");
+            var $listUnitDiv_bigCardDiv = $("<div class='bigCard'></div>");
+            var $bigCardDiv_A = $("<a></a>");
+            var $A_itemImg = $("<img>");
+            // 设置属性
+            $A_itemImg.attr({
+                class : "itemImg",
+                src : result[i].imgurl
+            });
+            // 将 img 插入 a 标签中
+            $bigCardDiv_A.append($A_itemImg);
+
+            // 创建 itemInfo
+            var $bigCardDiv_itemInfoDiv = $("<div class='itemInfo'></div>");
+
+
+
+        }
+
+
+    }
+})
