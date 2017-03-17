ef900291f53df3f6daa93d562be84cb037ff7ec4
mingfengst <mingfengst@163.com>
2017-03-17 11:23:33 +0800

    Commit by CMF

--- freeWorld/js/cityWalk.js
+++ freeWorld/js/cityWalk.js
@@ -19,47 +19,54 @@ $(function() {
                             $("<img>").attr({class: "itemImg", src: result[i].imgurl})
                         )
                     ).append(
-                        $("<div>").attr({class: "itemInfo"})
+                        $("<div>").attr({class: "itemInfo"}).append(
+                            $("<span>").attr({class: "infoPlace"}).text(result[i].address)
+                        ).append(
+                            $("<div>").attr({class: "infoNum"}).append(
+                                $("<span>").text(result[i].browseCount)
+                            ).append(
+                                "次浏览"
+                            ).append(
+                                $("<span>").text(result[i].soldCount)
+                            ).append(
+                                "件已售"
+                            )
+                        ).append(
+                            $("<h2>").append(
+                                $("<a>").attr({title: result[i].title, target: "_blank"}).text(result[i].title)
+                            )
+                        ).append(
+                            (function () {
+                                var $uls = $("<ul>").attr({class : "infoList"});
+                                for(var j = 0; j < result[i].introduce.length; j++){
+                                    $uls.append(
+                                        $("<li>").append(
+                                            $("<i>").attr({class : "iconfont icon-star-line"})
+                                        ).append(
+                                            result[i].introduce[j]
+                                        ).append(
+                                            $("<br>")
+                                        )
+                                    )
+                                }
+                                return $uls;
+                            })()
+                        ).append(
+                            $("<div>").attr({class : "bigCardPrice"}).append(
+                                $("<span>").attr({class : "line"}).text(result[i].oldPrice)
+                            ).append(
+                                $("<em>").text(result[i].newPrice)
+                            ).append(
+                                "元起"
+                            )
+                        ).append(
+                            $("<div>").attr({class : "bigCardBottomBar"}).append(
+                                $("<a>").attr({class : "bigCardBtn",target: "_blank"}).text("立即预订")
+                            )
+                        )
                     )
                 )
             )
-
-
-
-
-			$(".ProductList").append(($("<div>").attr({class: "listUnit"})
-                .append($("<div>").attr({class: "bigCard"})
-                    .append($("<a>")
-                        .append($("<img>").attr({class: "itemImg", src: result[i].imgurl})
-                        )
-                    )
-                    .append($("<div>").attr({class: "itemInfo"})
-                        .append($("<span>").attr({class: "infoPlace"}).text(result[i].address))
-                        .append($("<div>").attr({class: "infoNum"})
-                            .append($("<span>").append("次浏览").text(result[i].browseCount))
-                            .append($("<span>").append("件已售").text(result[i].soldCount))
-                        )
-                        .append($("<h2>")
-                            .append($("<a>").attr({title: result[i].title, target: "_blank"}).text(result[i].title))
-                        )
-                        .append($("<ul>")).attr({class : "infoList"})
-
-                                (function () {
-                                    for(var j = 0; j < result[i].introduce.length; j++){
-                                    $(".infoList").append(
-                                        $("li").append(
-                                            $("<i>").attr({class : "iconfont icon-star-line"}).text(result[i].introduce[j]).append("<br>")
-                                        ))
-                                    }
-                                })()
-
-                        .append($("div")).attr({class : "bigCardPrice"})
-                            .append($("<span>")).attr({class : "line"}).text(result[i].oldPrice)
-                            .append($("<em>")).text(result[i].newPrice).append("元起")
-                        .append($("div")).attr({class : "bigCardBottomBar"}).append($("<a>")).attr({class : "bigCardBtn",target: "_blank"}).text("立即预订")
-                    )
-                )
-            ))
 		}
 	}
 })
